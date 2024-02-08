4370e321c6d79efa182577a65588b37a93c4fe3e
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-02-08 21:50:33 +0300

    Initial commit - 1

---
+++ src/vucutKutleIndeksi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class vucutKutleIndeksi {
+    public static void main(String[] args) {
+        double boy, kilo;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lütfen boyunuz metre cinsinden giriniz.");
+        boy = sc.nextDouble();
+        System.out.println("Lütfen kilonuzu kg cinsinden giriniz.");
+        kilo = sc.nextDouble();
+        double indeks = kilo/(boy*boy);
+        System.out.println("Vücut kitle İndeksiniz: "+indeks);
+    }
+}
