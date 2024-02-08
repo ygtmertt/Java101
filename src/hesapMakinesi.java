57a9c2ec3c88a4fff4a57f955eacfe072e79e26a
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-02-08 23:54:14 +0300

    Initial commit - 1

---
+++ src/hesapMakinesi.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class hesapMakinesi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, select;
+        System.out.println("İlk sayıyı giriniz.");
+        n1 = sc.nextInt();
+        System.out.println("İkinci sayıyı giriniz.");
+        n2 = sc.nextInt();
+        System.out.println("İşlemi seçiniz.");
+        System.out.println("1-Toplama\n" +
+                "2-Çıkarma\n" +
+                "3-Çarpma\n" +
+                "4-Bölme");
+        select = sc.nextInt();
+        switch (select){
+            case 1:
+                System.out.println(n1+n2);
+                break;
+            case 2:
+                System.out.println(n1-n2);
+                break;
+            case 3:
+                System.out.println(n1*n2);
+                break;
+            case 4:
+                System.out.println(n1/n2);
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+    }
+}
