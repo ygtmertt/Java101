9b722fde234bf79b0a841ee97d8cb45ff64f8de0
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-02-08 21:43:10 +0300

    Initial commit - 1

---
+++ src/DaireninAlaniCevresi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DaireninAlaniCevresi {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Yarıçapı giriniz");
+        int r = sc.nextInt();
+        double pi = 3.14;
+        double alan = pi*r*r;
+        double cevre = 2*pi*r;
+        System.out.println("Dairenin alanı: "+alan+"cm2");
+        System.out.println("Dairenin çevresi: "+cevre+"cm");
+        System.out.println("Merkez Açı Ölçüsü giriniz.");
+        int a = sc.nextInt();
+        double dilimAlan = (pi*(r*r)*a)/360;
+        System.out.println("Daire Diliminin alanı: "+dilimAlan+"cm2");
+    }
+}
