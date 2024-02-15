519ba8b15294f55e133f374eca51ad50d1e3e46f
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-02-15 20:21:29 +0300

    Initial commit - 1

--- src/Loops/ciftSayiToplam.java
+++ src/Loops/ciftSayiToplam.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class ciftSayiToplam {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int sum = 0, count = 0;
         int input;
-        System.out.println("Enter a number.");
+        System.out.println("Bir sayı girin.");
         input = sc.nextInt();
-        for (int i = 0; i<=input; i++) {
-            if (i%2!=0) {
-                continue;
+        for (int i=0; i<=input; i++) {
+            if(i%3==0 && i%4==0) {
+                sum+=i;
+                count++;
             }
             else {
-                System.out.println(i);
+                continue;
             }
+        }
+        if (count>0) {
+            double ort = sum/count;
+            System.out.println("Girdiğiniz sayıdan 0'a kadar 3'e ve 4'e kalansız bölünen sayıların ortalaması: "+ort);
+        }
+        else {
+            System.out.println("Girdiğiniz sayıdan 0'a kadar 3'e ve 4'e kalansız bölünen sayı bulunamamıştır.");
         }
     }
 }
