8c6b9b5f2d65db1bccf1aa0dfedd7c31b3597378
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-05-23 18:49:04 +0300

    Initial commit - 1

--- src/Loops/EbobEkok.java
+++ src/Loops/EbobEkok.java
@@ -5,44 +5,28 @@ import java.util.Scanner;
 public class EbobEkok {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Birinci sayıyı giriniz.");
+        int ebob = 0;
+        int ekok = 0;
+        System.out.print("Birinci sayıyı giriniz.");
         int num1 = s.nextInt();
         System.out.println("İkinci sayıyı giriniz.");
         int num2 = s.nextInt();
-        int ebob, ekok;
-        System.out.println("Ebob");
-        if (num1 > num2) {
-            for(int i = 1; i<=num1; i++) {
-                if(num1%i==0 && num2%i==0) {
-                    System.out.println(i);
-                }
+        System.out.println("Sayıların En Büyük Ortak Böleni");
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                ebob = i;
             }
         }
-        else {
-            for(int i = 1; i<=num2; i++) {
-                if(num1%i==0 && num2%i==0) {
-                    System.out.println(i);
-                }
+        System.out.println(ebob);
+
+
+        System.out.println("Sayıların En Küçük Ortak Katı");
+        for(int i = 1; i<=num1*num2; i++) {
+            if(i%num1==0 && i%num2==0) {
+                ekok=i;
+                break;
             }
         }
-        System.out.println("Ekok");
-        if(num1 > num2) {
-            for(int i = 1; i<=20; i++) {
-                for(int j = 1; j<=20; j++) {
-                    if(num1*i==num2*j) {
-                        System.out.println(num1*i);
-                    }
-                }
-            }
-        }
-        else {
-            for(int i = 1; i<=20; i++) {
-                for(int j = 1; j<=20; j++) {
-                    if(num2*i==num1*j) {
-                        System.out.println(num2*i);
-                    }
-                }
-            }
-        }
+        System.out.println(ekok);
     }
 }
