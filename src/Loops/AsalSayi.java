c6c4f797acf8ed18acc1b4530f602bff192691cb
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2024-06-13 15:40:07 +0300

    Initial commit - 1

---
+++ src/Loops/AsalSayi.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        System.out.println("1 ile 100 arasındaki Asal Sayılar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asal = true;  // Başta sayının asal olduğunu kabul edelim. (2 asal sayıdır)
+            for (int i = 2; i <= Math.sqrt(sayi); i++) {
+                if(sayi % i == 0) {
+                    asal = false;
+                    break;
+                }
+            }
+            if (asal) {
+                System.out.println(sayi+" ");
+            }
+        }
+    }
+}
